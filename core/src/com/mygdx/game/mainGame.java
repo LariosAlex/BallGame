4c2a8d42cf813d14127088a4cb4ebaf4f275e19f
Alex <alexlarios49@gmail.com>
2022-02-02 18:36:42 +0100

    main

--- core/src/com/mygdx/game/mainGame.java
+++ core/src/com/mygdx/game/mainGame.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,7 +14,6 @@ public class mainGame extends Game {
     @Override
     public void create() {
         batch = new SpriteBatch();
-
         font = new BitmapFont();
         this.setScreen(new BallGame1(this));
     }
--- html/src/com/mygdx/game/client/HtmlLauncher.java
+++ html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.mygdx.game.BallGame1;
+import com.mygdx.game.mainGame;
 
 public class HtmlLauncher extends GwtApplication {
-
         @Override
         public GwtApplicationConfiguration getConfig () {
                 // Resizable application, uses available space in browser
+                GwtApplicationConfiguration config = new GwtApplicationConfiguration();
                 return new GwtApplicationConfiguration(true);
                 // Fixed size application:
                 //return new GwtApplicationConfiguration(480, 320);
@@ -17,6 +18,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new BallGame1();
+                return new mainGame();
         }
 }
